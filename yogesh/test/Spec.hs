b079531c03cfad7b71a30dc484c151cc8ff3ca04
Yogesh Dhamija <ydhamija96@gmail.com>
2020-02-05 20:37:34 -0600

    Add hspec test

--- yogesh/test/Spec.hs
+++ yogesh/test/Spec.hs
@@ -1,2 +1,8 @@
+import Test.Hspec
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "The test file itself" $ do
+        it "should successfully build and decide that 1==1" $
+            1 `shouldBe` 1
